ee14b9396c26ef3312cd59c04b6c246fbb6d6e46
jakenugget <59673380+jakenugget@users.noreply.github.com>
2021-06-24 03:14:22 -0600

    Update bom.js

--- bom/js/bom.js
+++ bom/js/bom.js
@@ -4,15 +4,15 @@ const button = document.querySelector("button");
 
 button.addEventListener('click', () => {
     if (input.value != '' ) {
-        // Create the elements in the list
+        
         let li = document.createElement('li');
         let deletebutton = document.createElement('button');
 
-        // Change some properties' text content
+        
         li.textContent = input.value;
         deletebutton.textContent = '❌';
 
-        // add button to the li
+       
         li.append(deletebutton);
         output.append(li);
         deletebutton.addEventListener('click', () => {
